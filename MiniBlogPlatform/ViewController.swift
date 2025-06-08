59298494fb01c0c50d85ec26c13cc7acca701def
Salome <salomemsxvilidze@icloud.com>
2025-06-08 13:58:12 +0400

    Initial Commit

---
+++ MiniBlogPlatform/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniBlogPlatform
+//
+//  Created by Mcbook Pro on 08.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
