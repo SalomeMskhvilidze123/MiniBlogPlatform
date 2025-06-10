65ef8624d026a3038f6df7c17e0a9f128c7a0ff2
Salome <salomemsxvilidze@icloud.com>
2025-06-10 23:35:25 +0400

    chore: Organize project structure into folders

---
+++ MiniBlogPlatform/Networking/APIClient.swift
@@ -0,0 +1,16 @@
+//
+//  APIClient.swift
+//  MiniBlogPlatform
+//
+//  Created by Mcbook Pro on 10.06.25.
+//
+
+import Foundation
+
+class APIClient {
+    func fetchPosts(complition: ( Result<[Post],Error> )-> Void){
+        
+    }
+    
+
+}
